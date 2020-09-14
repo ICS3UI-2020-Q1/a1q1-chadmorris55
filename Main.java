6fa2f740475ea7d2b707bf4a0144fa7cd862df3d
morrc3611 <morrc3611@wrdsb.ca>
2020-09-14 13:36:04 +0000

    program that converts tempC to tempf

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 /**
- *
+ *This program will convert the temperature in degrees Celsius into degrees Farenheit 
  * @author 
  */
 public class Main {
@@ -9,7 +11,16 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    //asks user for the tempC
+    System.out.println("Please enter a temperature you would like to convert in degrees Celsius.");
+    // declare and initialize the temperature in Celsius
+    int tempC = input.nextInt();
+
+    // converts tempC to tempF
+    int tempF = (tempC * 9) / 5 + 32;
+    System.out.println(tempC + "C is the same as " + tempF + "F" );
   }
 }
